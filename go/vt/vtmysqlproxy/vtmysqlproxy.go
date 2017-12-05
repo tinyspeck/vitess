59d0dd362b3164547f8853c301623615b1d5f8d0
Michael Demmer <mdemmer@slack-corp.com>
2017-12-04 22:01:32 -0800

    add initial implementation of vtmysqlproxy
    Basic query support is working and tested with the mysql protocol
    handler.

---
+++ go/vt/vtmysqlproxy/vtmysqlproxy.go
@@ -0,0 +1,123 @@
+/*
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// vtmysqlproxy is a standalone version of the tablet server
+package vtmysqlproxy
+
+import (
+	"context"
+	"fmt"
+
+	log "github.com/golang/glog"
+
+	"github.com/youtube/vitess/go/sqltypes"
+	"github.com/youtube/vitess/go/vt/dbconfigs"
+	"github.com/youtube/vitess/go/vt/mysqlctl"
+	"github.com/youtube/vitess/go/vt/servenv"
+	"github.com/youtube/vitess/go/vt/tableacl"
+	"github.com/youtube/vitess/go/vt/tableacl/simpleacl"
+	"github.com/youtube/vitess/go/vt/vttablet/queryservice"
+	"github.com/youtube/vitess/go/vt/vttablet/tabletserver"
+	"github.com/youtube/vitess/go/vt/vttablet/tabletserver/tabletenv"
+
+	querypb "github.com/youtube/vitess/go/vt/proto/query"
+	topodatapb "github.com/youtube/vitess/go/vt/proto/topodata"
+)
+
+var (
+	mysqlProxy *MysqlProxy
+	target     = querypb.Target{
+		TabletType: topodatapb.TabletType_MASTER,
+		Keyspace:   "",
+	}
+)
+
+type MysqlProxy struct {
+	qs queryservice.QueryService
+}
+
+func (mp *MysqlProxy) Execute(ctx context.Context, session *MysqlProxySession, sql string, bindVariables map[string]*querypb.BindVariable) (*MysqlProxySession, *sqltypes.Result, error) {
+	result, err := mp.qs.Execute(ctx, &target, sql, bindVariables, session.transactionID, session.Options)
+	if err != nil {
+		return nil, nil, err
+	}
+	return session, result, nil
+}
+
+func (mp *MysqlProxy) Rollback(ctx context.Context, transactionID int64) error {
+	return nil
+}
+
+type MysqlProxySession struct {
+	transactionID int64
+	TargetString  string
+	Options       *querypb.ExecuteOptions
+}
+
+// Init initializes the proxy
+func Init(dbcfgs *dbconfigs.DBConfigs, tableACLConfig string) error {
+	log.Infof("initalizing vtmysqlproxy")
+
+	// creates and registers the query service
+	qs := tabletserver.NewTabletServerWithNilTopoServer(tabletenv.Config)
+
+	mysqlProxy = &MysqlProxy{
+		qs: qs,
+	}
+
+	servenv.OnRun(func() {
+		qs.Register()
+		//		qs.SetServingType(topodatapb.TabletType_MASTER, true, []topodatapb.TabletType{})
+		//		addStatusParts(qs)
+	})
+
+	servenv.OnClose(func() {
+		// We now leave the queryservice running during lameduck,
+		// so stop it in OnClose(), after lameduck is over.
+		qs.StopService()
+	})
+
+	if tableACLConfig != "" {
+		// To override default simpleacl, other ACL plugins must set themselves to be default ACL factory
+		tableacl.Register("simpleacl", &simpleacl.Factory{})
+
+		// tabletacl.Init loads ACL from file if *tableACLConfig is not empty
+		err := tableacl.Init(
+			tableACLConfig,
+			func() {
+				qs.ClearQueryPlanCache()
+			},
+		)
+		if err != nil {
+			return fmt.Errorf("Failed to initialize Table ACL: %v", err)
+		}
+	}
+
+	// XXX/demmer replace this with a thinner interface that doesn't include
+	// all of mysqlctl
+	mysqld := mysqlctl.NewMysqld(
+		&mysqlctl.Mycnf{},
+		dbcfgs,
+		dbconfigs.AppConfig,
+	)
+
+	err := qs.StartService(target, *dbcfgs, mysqld)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
