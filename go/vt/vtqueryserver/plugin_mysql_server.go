1d7fd4d8829210b5807391d74acd38439ac135d1
Jamie Scheinblum <js@slack-corp.com>
2017-12-15 13:29:11 -0800

    Add missing handler

--- go/vt/vtqueryserver/plugin_mysql_server.go
+++ go/vt/vtqueryserver/plugin_mysql_server.go
@@ -75,6 +75,10 @@ func (mh *proxyHandler) ConnectionClosed(c *mysql.Conn) {
 	}
 }
 
+func (vh *proxyHandler) ConnectionNegotiated(c *mysql.Conn) error {
+	return nil
+}
+
 func (mh *proxyHandler) ComQuery(c *mysql.Conn, query string, callback func(*sqltypes.Result) error) error {
 	// FIXME(alainjobart): Add some kind of timeout to the context.
 	ctx := context.Background()
