9fac7f841f41da3c0fc7d37dc35233aaf7848fd8
Richard Bailey <rbailey@slack-corp.com>
2019-06-05 09:37:47 -0700

    Cherry-pick: suppress begin...commit in autocommit logs
    Original Message:
    The previous (untested) implementation turned out to be in the wrong
    place in the TabletServer execution tier and did not properly log
    the actual statements being executed.
    
    Implement this the right way by returning the statements that were
    really executed out from the TxPool, then using those to determine
    whether or not to log the statement.
    
    Original PR: vitessio#4827

--- go/vt/vttablet/tabletserver/query_executor.go
+++ go/vt/vttablet/tabletserver/query_executor.go
@@ -296,11 +296,14 @@ func (qre *QueryExecutor) execDmlAutoCommit() (reply *sqltypes.Result, err error
 }
 
 func (qre *QueryExecutor) execAsTransaction(f func(conn *TxConnection) (*sqltypes.Result, error)) (reply *sqltypes.Result, err error) {
-	conn, err := qre.tsv.te.txPool.LocalBegin(qre.ctx, qre.options)
+	conn, beginSQL, err := qre.tsv.te.txPool.LocalBegin(qre.ctx, qre.options)
 	if err != nil {
 		return nil, err
 	}
 	defer qre.tsv.te.txPool.LocalConclude(qre.ctx, conn)
+	if beginSQL != "" {
+		qre.logStats.AddRewrittenSQL(beginSQL, time.Now())
+	}
 	qre.logStats.AddRewrittenSQL("begin", time.Now())
 
 	reply, err = f(conn)
@@ -311,8 +314,12 @@ func (qre *QueryExecutor) execAsTransaction(f func(conn *TxConnection) (*sqltype
 		qre.logStats.AddRewrittenSQL("rollback", start)
 		return nil, err
 	}
-	err = qre.tsv.te.txPool.LocalCommit(qre.ctx, conn, qre.tsv.messager)
-	qre.logStats.AddRewrittenSQL("commit", start)
+
+	commitSQL, err := qre.tsv.te.txPool.LocalCommit(qre.ctx, conn, qre.tsv.messager)
+	// As above LocalCommit is a no-op for autocommmit so don't log anything.
+	if commitSQL != "" {
+		qre.logStats.AddRewrittenSQL("commit", start)
+	}
 	if err != nil {
 		return nil, err
 	}
