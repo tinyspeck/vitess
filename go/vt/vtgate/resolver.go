318ce281e58e69840b1fb9fb434ad665d66e01da
Rafael Chacon <rafael@slack-corp.com>
2020-03-09 18:58:31 -0700

    Use proper session to propagate autocommit information
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>

--- go/vt/vtgate/resolver.go
+++ go/vt/vtgate/resolver.go
@@ -83,7 +83,7 @@ func (res *Resolver) Execute(
 	keyspace string,
 	tabletType topodatapb.TabletType,
 	destination key.Destination,
-	session *vtgatepb.Session,
+	session *SafeSession,
 	notInTransaction bool,
 	options *querypb.ExecuteOptions,
 	logStats *LogStats,
@@ -97,8 +97,7 @@ func (res *Resolver) Execute(
 		logStats.ShardQueries = uint32(len(rss))
 	}
 
-	safeSession := NewSafeSession(session)
-	autocommit := len(rss) == 1 && safeSession.AutocommitApproval()
+	autocommit := len(rss) == 1 && session.AutocommitApproval()
 
 	for {
 		qr, err := res.scatterConn.Execute(
@@ -107,7 +106,7 @@ func (res *Resolver) Execute(
 			bindVars,
 			rss,
 			tabletType,
-			safeSession,
+			session,
 			notInTransaction,
 			options,
 			autocommit,
