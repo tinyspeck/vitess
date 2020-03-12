038a4fded51b8f4251520850083c95b5d7e0d732
Rafael Chacon <rafael@slack-corp.com>
2020-03-12 11:47:12 -0700

    Fixes bug caught by test, update test to reflect new behavior
    Signed-off-by: Rafael Chacon <rafael@slack-corp.com>

--- go/vt/vtgate/autocommit_test.go
+++ go/vt/vtgate/autocommit_test.go
@@ -20,6 +20,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"golang.org/x/net/context"
 
 	"vitess.io/vitess/go/sqltypes"
@@ -389,7 +390,7 @@ func TestAutocommitTransactionStarted(t *testing.T) {
 	testCommitCount(t, "sbc1", sbc1, 0)
 }
 
-// TestAutocommitDirectTarget: no instant-commit.
+// TestAutocommitDirectTarget: instant-commit.
 func TestAutocommitDirectTarget(t *testing.T) {
 	executor, _, _, sbclookup := createExecutorEnv()
 
@@ -400,15 +401,15 @@ func TestAutocommitDirectTarget(t *testing.T) {
 	}
 	sql := "insert into simple(val) values ('val')"
 
-	if _, err := executor.Execute(context.Background(), "TestExecute", NewSafeSession(session), sql, map[string]*querypb.BindVariable{}); err != nil {
-		t.Error(err)
-	}
-	testQueries(t, "sbclookup", sbclookup, []*querypb.BoundQuery{{
+	_, err := executor.Execute(context.Background(), "TestExecute", NewSafeSession(session), sql, map[string]*querypb.BindVariable{})
+	require.NoError(t, err)
+
+	testBatchQuery(t, "sbclookup", sbclookup, &querypb.BoundQuery{
 		Sql:           sql + "/* vtgate:: filtered_replication_unfriendly */",
 		BindVariables: map[string]*querypb.BindVariable{},
-	}})
-	testAsTransactionCount(t, "sbclookup", sbclookup, 0)
-	testCommitCount(t, "sbclookup", sbclookup, 1)
+	})
+	testAsTransactionCount(t, "sbclookup", sbclookup, 1)
+	testCommitCount(t, "sbclookup", sbclookup, 0)
 }
 
 // TestAutocommitDirectRangeTarget: no instant-commit.
