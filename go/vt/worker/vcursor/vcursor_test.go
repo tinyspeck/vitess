f93eee01ba9bba992e6b807f5fd18e5a932ef185
Brian Ramos <brirams@users.noreply.github.com>
2020-07-30 18:09:22 -0700

    generate mock implementation of vtgateconn.Impl and use it for tests for vtworker vcursor. Generated mock with:
    'mockgen -self_package vitess.io/vitess/go/vt/vtgate/vtgateconn -package vtgateconn -destination mock_impl.go vitess.io/vitess/go/vt/vtgate/vtgateconn Impl'
    
    Signed-off-by: Brian Ramos <brirams@users.noreply.github.com>

--- go/vt/worker/vcursor/vcursor_test.go
+++ go/vt/worker/vcursor/vcursor_test.go
@@ -17,19 +17,135 @@ limitations under the License.
 package vcursor
 
 import (
-	"strings"
 	"testing"
 
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
+
+	"vitess.io/vitess/go/sqltypes"
+	"vitess.io/vitess/go/vt/vtgate/vindexes"
+	"vitess.io/vitess/go/vt/vtgate/vtgateconn"
+
+	vtgatepb "vitess.io/vitess/go/vt/proto/vtgate"
 )
+
+// Since we're not invoking the package registration, we mimic it here.
+func registerGrpcDialer(t *testing.T, expectedAddr string, impl vtgateconn.Impl) {
+	vtgateconn.RegisterDialer(
+		"grpc",
+		func(c context.Context, address string) (vtgateconn.Impl, error) {
+			assert.Equal(t, expectedAddr, address)
+			assert.Equal(t, context.Background(), c)
+
+			return impl, nil
+		},
+	)
+}
+
+// Make some basic assertions on a call to NewVCursor that expected to return non-error
+// results
+func getAndAssertNewVCursor(
+	t *testing.T,
+	ctx context.Context,
+	hostPortString, targetString string,
+) (vindexes.VCursor, CleanupFunc) {
+	vcursor, closeFn, err := NewVCursor(ctx, hostPortString, targetString)
+	assert.Nil(t, err)
+	assert.NotNil(t, vcursor)
+	assert.NotNil(t, closeFn)
+
+	return vcursor, closeFn
+}
 
 func TestVCursorWithNoDialer(t *testing.T) {
 	_, _, err := NewVCursor(context.Background(), "doesn't matter", "really doesn't matter")
-	if err == nil {
-		t.Fatal("Error expected but not returned")
-	}
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "no dialer registered")
+}
 
-	if !strings.Contains(err.Error(), "no dialer registered") {
-		t.Fatalf("Unexpected error returned: %s", err)
+func TestVCursorCloseFn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mi := vtgateconn.NewMockImpl(ctrl)
+
+	expectedAddr := "255.255.255.255"
+	registerGrpcDialer(t, expectedAddr, mi)
+	_, closeFn := getAndAssertNewVCursor(
+		t,
+		context.Background(),
+		"255.255.255.255",
+		"really doesn't matter",
+	)
+
+	mi.EXPECT().Close()
+	closeFn()
+}
+
+func TestVCursorExecuteKeyspaceIDReturnsErr(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mi := vtgateconn.NewMockImpl(ctrl)
+
+	expectedAddr := "255.255.255.255"
+	registerGrpcDialer(t, expectedAddr, mi)
+	vc, _ := getAndAssertNewVCursor(
+		t,
+		context.Background(),
+		"255.255.255.255",
+		"really doesn't matter",
+	)
+
+	res, err := vc.ExecuteKeyspaceID(
+		"keyspace",
+		nil,
+		"query",
+		nil,
+		false,
+		false,
+	)
+
+	assert.Nil(t, res)
+	assert.Contains(
+		t,
+		err.Error(),
+		"Unexpected call to `ExecuteKeyspaceId` for keyspace keyspace",
+	)
+}
+
+func TestVCursorExecute(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mi := vtgateconn.NewMockImpl(ctrl)
+
+	registerGrpcDialer(t, "255.255.255.255", mi)
+	vc, _ := getAndAssertNewVCursor(
+		t,
+		context.Background(),
+		"255.255.255.255",
+		"really doesn't matter",
+	)
+
+	expectedRes := &sqltypes.Result{
+		RowsAffected: 100,
+		InsertID:     123,
 	}
+	mi.EXPECT().Execute(
+		gomock.Eq(context.Background()),
+		gomock.Any(),
+		gomock.Eq("query"),
+		nil,
+	).Return(nil, expectedRes, nil)
+
+	actualRes, err := vc.Execute(
+		"method",
+		"query",
+		nil,   // bindVars map[string]*querypb.BindVariable,
+		false, // isDML
+		vtgatepb.CommitOrder_NORMAL,
+	)
+
+	assert.NotNil(t, actualRes)
+	assert.Nil(t, err)
+	assert.Same(t, expectedRes, actualRes)
 }
