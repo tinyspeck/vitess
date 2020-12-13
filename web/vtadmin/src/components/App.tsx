081748105c12b78d9f7c5a617752b1353d7fc969
Sara Bee <855595+doeg@users.noreply.github.com>
2020-12-13 16:42:40 -0500

    Add react-query + a pleasantly hideous tablet table
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/components/App.tsx
+++ web/vtadmin/src/components/App.tsx
@@ -1,12 +1,33 @@
-import logo from '../img/logo.svg';
-import style from './App.module.scss';
+import { useTablets } from "../hooks/apiQueries";
+import logo from "../img/logo.svg";
+import style from "./App.module.scss";
 
 export const App = () => {
+  const tq = useTablets();
+  const tablets = tq.data || [];
+
   return (
     <div className={style.container}>
       <img src={logo} className="App-logo" alt="logo" width={200} />
       <h1>VTAdmin</h1>
-      <code>{process.env.REACT_APP_VTADMIN_API_ADDRESS}</code>
+      <table className={style.table}>
+        <thead>
+          <tr>
+            <th>Cluster</th>
+            <th>Hostname</th>
+            <th>Keyspace</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tablets.map((t, idx) => (
+            <tr key={idx}>
+              <td>{t.cluster?.name}</td>
+              <td>{t.tablet?.hostname}</td>
+              <td>{t.tablet?.keyspace}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
