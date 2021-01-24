b7b5a2d0cca974e96a842f6cd9086dc2b2bcbd39
Rohit Nayak <57520317+rohit-nayak-ps@users.noreply.github.com>
2021-01-24 10:05:09 +0100

    Merge pull request #7360 from tinyspeck/am_vtctld_getvschema
    [vtctld] Migrate GetVSchema to VtctldServer

--- go/vt/vtctl/grpcvtctldclient/client_gen.go
+++ go/vt/vtctl/grpcvtctldclient/client_gen.go
@@ -126,3 +126,12 @@ func (client *gRPCVtctldClient) GetTablets(ctx context.Context, in *vtctldatapb.
 
 	return client.c.GetTablets(ctx, in, opts...)
 }
+
+// GetVSchema is part of the vtctlservicepb.VtctldClient interface.
+func (client *gRPCVtctldClient) GetVSchema(ctx context.Context, in *vtctldatapb.GetVSchemaRequest, opts ...grpc.CallOption) (*vtctldatapb.GetVSchemaResponse, error) {
+	if client.c == nil {
+		return nil, status.Error(codes.Unavailable, connClosedMsg)
+	}
+
+	return client.c.GetVSchema(ctx, in, opts...)
+}
