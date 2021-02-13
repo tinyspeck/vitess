efd7688b0eaba7a6ee6038cdff8ce2c0d3e8c22a
Sara Bee <855595+doeg@users.noreply.github.com>
2021-02-13 09:18:32 -0500

    Return  as nested property from useURLQuery
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/hooks/useURLQuery.ts
+++ web/vtadmin/src/hooks/useURLQuery.ts
@@ -16,23 +16,33 @@
 import qs from 'query-string';
 import { useLocation } from 'react-router-dom';
 
-/**
- * useURLQuery is a hook for parsing query parameters from the current URL
- * into a map, where "query parameters" are those appearing after the "?".
- *
- * So, given a URL like: https://test.com/some/route?foo=bar&count=123
- *                                                   ^^^^^^^^^^^^^^^^^
- * ... useURLQuery() would return `{ foo: "bar", count: 123 }`
- */
-export const useURLQuery = () => {
+export interface ParsedQuery<T = string> {
+    [key: string]: T | T[] | null;
+}
+
+// useURLQuery is a hook for getting and setting query parameters from the current URL,
+// where "query parameters" are those appearing after the "?":
+//
+//      https://test.com/some/route?foo=bar&count=123
+//                                  ^^^^^^^^^^^^^^^^^
+//
+// The query parameters from the above URL would be parsed as:
+//
+//      { foo: "bar", count: 123 }
+//
+export const useURLQuery = (): {
+    query: ParsedQuery<string | number | boolean>;
+} => {
     const { search } = useLocation();
 
     // For full options, see: https://github.com/sindresorhus/query-string
-    return qs.parse(search, {
+    const query = qs.parse(search, {
         // Parse arrays with elements using duplicate keys
         // 'foo=1&foo=2&foo=3' => { foo: [1, 2, 3] }
         arrayFormat: 'none',
         parseBooleans: true,
         parseNumbers: true,
     });
+
+    return { query };
 };
