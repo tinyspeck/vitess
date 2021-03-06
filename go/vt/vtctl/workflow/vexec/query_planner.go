1f6f3226768ba5132c13819098437775d50c264f
Richard Bailey <rbailey@slack-corp.com>
2021-03-05 17:30:42 -0800

    Resolve merge / backport conflicts
    Signed-off-by: Richard Bailey <rbailey@slack-corp.com>

--- go/vt/vtctl/workflow/vexec/query_planner.go
+++ go/vt/vtctl/workflow/vexec/query_planner.go
@@ -265,14 +265,14 @@ func addDefaultWheres(planner QueryPlanner, where *sqlparser.Where) *sqlparser.W
 			Left: &sqlparser.ColName{
 				Name: sqlparser.NewColIdent(params.DBNameColumn),
 			},
-			Operator: sqlparser.EqualStr,
-			Right:    sqlparser.NewStrVal([]byte(params.DBName)),
+			Operator: sqlparser.EqualOp,
+			Right:    sqlparser.NewStrLiteral([]byte(params.DBName)),
 		}
 
 		switch newWhere {
 		case nil:
 			newWhere = &sqlparser.Where{
-				Type: sqlparser.WhereStr,
+				Type: sqlparser.WhereClause,
 				Expr: expr,
 			}
 		default:
@@ -288,8 +288,8 @@ func addDefaultWheres(planner QueryPlanner, where *sqlparser.Where) *sqlparser.W
 			Left: &sqlparser.ColName{
 				Name: sqlparser.NewColIdent(params.WorkflowColumn),
 			},
-			Operator: sqlparser.EqualStr,
-			Right:    sqlparser.NewStrVal([]byte(params.Workflow)),
+			Operator: sqlparser.EqualOp,
+			Right:    sqlparser.NewStrLiteral([]byte(params.Workflow)),
 		}
 
 		newWhere.Expr = &sqlparser.AndExpr{
