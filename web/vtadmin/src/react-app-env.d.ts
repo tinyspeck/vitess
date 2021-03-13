35b805e36b0ed51d49a123af620641b9fb454c72
Sara Bee <855595+doeg@users.noreply.github.com>
2021-03-13 17:21:02 -0500

    tablet links
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/react-app-env.d.ts
+++ web/vtadmin/src/react-app-env.d.ts
@@ -12,6 +12,10 @@ declare namespace NodeJS {
         // made against vtadmin-api. If unspecified, uses fetch defaults.
         // See https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#sending_a_request_with_credentials_included
         REACT_APP_FETCH_CREDENTIALS?: RequestCredentials;
+
+        // Optional but strongly recommended.
+        // Example: "https://{{hostname}}.foo.test.com:1234"
+        REACT_APP_TABLET_LINK_TEMPLATE?: string;
     }
 }
 
