b669ab387ae77884db97499b1f1c3c4b21d4e73d
Andrew Mason <amason@slack-corp.com>
2021-06-15 08:17:01 -0400

    Restore `grpcvtctldserver` tests
    Steps taken:
    1. `git checkout upstream/main -- go/vt/vtctl/grpcvtctldserver/server_test.go`
    2. `git checkout upstream/main -- go/vt/vtctl/grpcvtctldserver/testutil`
    3. Replace imports of `google.golang.org/protobuf/proto` with `github.com/golang/protobuf/proto`, as we haven't taken the protobuf upgrade yet
    4. Remove all types referenced in the tests that we haven't added to vtctldatapb yet
    
    Note: at the time, `upstream/main` resolved to `83dfb9aa59ce47a313ecb7bc12804298e76d350c`.
    
    Signed-off-by: Andrew Mason <amason@slack-corp.com>

--- go/vt/vtctl/grpcvtctldserver/testutil/proto_compare.go
+++ go/vt/vtctl/grpcvtctldserver/testutil/proto_compare.go
@@ -17,30 +17,87 @@ limitations under the License.
 package testutil
 
 import (
+	"fmt"
+	"sort"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 
+	"vitess.io/vitess/go/test/utils"
+
+	logutilpb "vitess.io/vitess/go/vt/proto/logutil"
+	topodatapb "vitess.io/vitess/go/vt/proto/topodata"
 	vtctldatapb "vitess.io/vitess/go/vt/proto/vtctldata"
 )
+
+// AssertEmergencyReparentShardResponsesEqual asserts that two
+// vtctldatapb.EmergencyReparentShardResponse objects are equal, ignoring their
+// respective Events field in the comparison.
+func AssertEmergencyReparentShardResponsesEqual(t *testing.T, expected *vtctldatapb.EmergencyReparentShardResponse, actual *vtctldatapb.EmergencyReparentShardResponse, msgAndArgs ...interface{}) {
+	t.Helper()
+
+	expected = proto.Clone(expected).(*vtctldatapb.EmergencyReparentShardResponse)
+	expected.Events = nil
+
+	actual = proto.Clone(actual).(*vtctldatapb.EmergencyReparentShardResponse)
+	actual.Events = nil
+
+	utils.MustMatch(t, expected, actual)
+}
+
+func AssertSameTablets(t *testing.T, expected, actual []*topodatapb.Tablet) {
+	sort.Slice(expected, func(i, j int) bool {
+		return fmt.Sprintf("%v", expected[i]) < fmt.Sprintf("%v", expected[j])
+	})
+	sort.Slice(actual, func(i, j int) bool {
+		return fmt.Sprintf("%v", actual[i]) < fmt.Sprintf("%v", actual[j])
+	})
+	utils.MustMatch(t, expected, actual)
+}
 
 // AssertKeyspacesEqual is a convenience function to assert that two
 // vtctldatapb.Keyspace objects are equal, after clearing out any reserved
 // proto XXX_ fields.
 func AssertKeyspacesEqual(t *testing.T, expected *vtctldatapb.Keyspace, actual *vtctldatapb.Keyspace, msgAndArgs ...interface{}) {
 	t.Helper()
+	utils.MustMatch(t, expected, actual)
+}
 
-	for _, ks := range []*vtctldatapb.Keyspace{expected, actual} {
-		if ks.Keyspace != nil {
-			ks.Keyspace.XXX_sizecache = 0
-			ks.Keyspace.XXX_unrecognized = nil
-		}
+// AssertLogutilEventsOccurred asserts that for something containing a slice of
+// logutilpb.Event, that the container is non-nil, and the event slice is
+// non-zero.
+//
+// This test function is generalized with an anonymous interface that any
+// protobuf type containing a slice of logutilpb.Event elements called Events,
+// which is the convention in protobuf types in the Vitess codebase, already
+// implements.
+func AssertLogutilEventsOccurred(t *testing.T, container interface{ GetEvents() []*logutilpb.Event }, msgAndArgs ...interface{}) {
+	t.Helper()
 
-		if ks.Keyspace.SnapshotTime != nil {
-			ks.Keyspace.SnapshotTime.XXX_sizecache = 0
-			ks.Keyspace.SnapshotTime.XXX_unrecognized = nil
-		}
+	if container == nil {
+		assert.Fail(t, "Events container must not be nil", msgAndArgs...)
+
+		return
 	}
 
-	assert.Equal(t, expected, actual, msgAndArgs...)
+	assert.Greater(t, len(container.GetEvents()), 0, msgAndArgs...)
+}
+
+// AssertNoLogutilEventsOccurred asserts that for something containing a slice
+// of logutilpb.Event, that the container is either nil, or that the event slice
+// is exactly zero length.
+//
+// This test function is generalized with an anonymous interface that any
+// protobuf type containing a slice of logutilpb.Event elements called Events,
+// which is the convention in protobuf types in the Vitess codebase, already
+// implements.
+func AssertNoLogutilEventsOccurred(t *testing.T, container interface{ GetEvents() []*logutilpb.Event }, msgAndArgs ...interface{}) {
+	t.Helper()
+
+	if container == nil {
+		return
+	}
+
+	assert.Equal(t, len(container.GetEvents()), 0, msgAndArgs...)
 }
